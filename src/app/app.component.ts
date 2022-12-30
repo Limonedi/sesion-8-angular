8586a9dca5edb358efeae3ff921313c11dee7602
Limonedi <holuloldoce@gmail.com>
2022-12-30 10:15:05 -0500

    tare8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tare8';
-    usuario = 'Angular';
-    password = '123456';
-    isLoggedIn = false;
-  
-    checkCredentials() {
-      if (this.username === this.usuario && this.password === this.password) {
-        this.isLoggedIn = true;
-      } else {
-        this.isLoggedIn = false;
-      }
-    }
-  }
-  
 
+  personas: string;
+  nombre: string;
+  edad: number;
+  apellidos:string;
+  
+  usuario = 'Angular';
+  appPassword = '123456';
+  username: string;
+  loginPassword: string;
+  
+  isLoggedIn = false;
+  links = [
+    {
+      title: 'Inicio',
+      url: '/home'
+    },
+    {
+      title: 'Acerca de',
+      url: '/about'
+    },
+    {
+      title: 'Contacto',
+      url: '/contact'
+    }
+  ];
+
+  checkCredentials() {
+    if (this.username === this.usuario && this.loginPassword === this.appPassword) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+  } 
+  }}
