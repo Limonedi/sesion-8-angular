f3f87ff3908bffea6598312503851ba4163668dd
Limonedi <holuloldoce@gmail.com>
2022-12-30 09:44:59 -0500

    tare8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tare8';
-}
+    usuario = 'Angular';
+    password = '123456';
+    isLoggedIn = false;
+  
+    checkCredentials() {
+      if (this.username === this.usuario && this.password === this.password) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    }
+  }
+  
+
